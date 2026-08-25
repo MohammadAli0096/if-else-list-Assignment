aa0af217022eeee9560af4f2599951be053baf65
MohammadAli0096 <155341782+MohammadAli0096@users.noreply.github.com>
2026-08-25 11:04:58 +0500

    Add files via upload

---
+++ Q_2.dart
@@ -0,0 +1,11 @@
+// Q.2: Take two variables and store age then using if/else condition to determine oldest and youngest among them.
+
+void main() {
+  int ali = 30;
+  int ahmed = 25;
+  if (ali > ahmed) {
+    print("Ali is older then Ahmed");
+  } else {
+    print("Ahmed is older then Ali");
+  }
+}
