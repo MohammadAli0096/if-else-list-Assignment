df039e10b62c3f4b6c2cd1b322cf51a803dd09a0
MohammadAli0096 <155341782+MohammadAli0096@users.noreply.github.com>
2026-08-28 12:48:34 +0500

    Add files via upload

---
+++ Q_10.dart
@@ -0,0 +1,27 @@
+// Q: Create a list of Days and remove one by one from the end of list.
+void main() {
+  List<String> days = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+  print("Days of the week: $days");
+  days.removeLast();
+  print("Days of the week after removing last day: $days");
+  days.removeLast();
+  print("Days of the week after removing last day: $days");
+  days.removeLast();
+  print("Days of the week after removing last day: $days");
+  days.removeLast();
+  print("Days of the week after removing last day: $days");
+  days.removeLast();
+  print("Days of the week after removing last day: $days");
+  days.removeLast();
+  print("Days of the week after removing last day: $days");
+  days.removeLast();
+  print("Days of the week after removing last day: $days");
+}
