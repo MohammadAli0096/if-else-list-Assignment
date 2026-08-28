7038914e0ae9316e6086368edf7aaec21413efdd
MohammadAli0096 <155341782+MohammadAli0096@users.noreply.github.com>
2026-08-28 12:33:23 +0500

    Add files via upload

---
+++ Q_8.dart
@@ -0,0 +1,5 @@
+// Q: Create a list of names and print all names using list.
+void main() {
+  List<String> names = ["Ali", "Ahmed", "Ayesha", "Fatima", "Hassan"];
+  print("List of Names: $names");
+}
